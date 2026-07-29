a1d0e75432aefccd7bf21bb168d745970783cb48
Yuliia <demkovych.yuliia@gmail.com>
2026-07-29 22:14:43 +0300

    JS conditions and loops

---
+++ conditionsAndLoops/task3.js
@@ -0,0 +1,17 @@
+//Завдання 3: Генерація таблиці множення 
+
+//Використовуючи цикл for
+const number = 2;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(number + " x " + i + " = " + number * i);
+}
+
+//Використовуючи цикл while
+const number = 3;
+let i = 1;
+
+while (i <= 10) {
+    console.log(number + " x " + i + " = " + number * i);
+    i++;
+}
